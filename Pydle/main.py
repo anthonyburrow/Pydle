37254941f1059236f2fa5c48c996edce9434160f
Anthony Burrow <anthony.r.burrow@gmail.com>
2025-08-11 16:35:45 -0500

    start work on reset

--- Pydle/main.py
+++ Pydle/main.py
@@ -1,27 +1,54 @@
-from platformdirs import user_data_dir
+import argparse
 from pathlib import Path
+from platformdirs import user_data_dir
 
+from .commands.CommandRegistry import COMMAND_REGISTRY
 from .util.player.Player import Player
 from .util.structures.Controller import Controller
 from .util.structures.UserInterface import UserInterface
-from .commands.CommandRegistry import COMMAND_REGISTRY
 
 
 APP_NAME = 'Pydle'
 
 
 def main():
+    # Get any arguments to script
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--new',
+        action='store_true',
+        help='Delete save file and start new character'
+    )
+
+    args = parser.parse_args()
+
+    # Setup UI
+    ui: UserInterface = UserInterface()
+
     # Setup/load player
     path_save: Path = Path(user_data_dir(APP_NAME))
     path_save.mkdir(parents=True, exist_ok=True)
 
+    if args.new:
+        ui.print((
+            'Are you sure you want to make a new character? [Y/n]'
+            'This will delete your previous file.'
+        ))
+        answer: str = ui.get_input.lower()
+
+        if answer in ('y', 'yes'):
+            #delete
+            pass
+        elif answer in ('n', 'no'):
+            pass
+        else:
+            ui.print('Unknown response')
+
     character_file: str = str(path_save / 'player.json')
     player: Player = Player(save_file=character_file)
 
+    # Load command metadata
     COMMAND_REGISTRY.load_commands()
-
-    # Setup UI
-    ui: UserInterface = UserInterface()
 
     # Setup game controller
     controller: Controller = Controller(player, ui)
