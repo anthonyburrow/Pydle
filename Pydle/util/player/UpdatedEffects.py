d13ce34426f7154f6c43e3dc326c73d68bc209cf
anthonyburrow <anthony.r.burrow@gmail.com>
2025-08-13 23:45:33 -0500

    clean up updated effects

--- Pydle/util/player/UpdatedEffects.py
+++ Pydle/util/player/UpdatedEffects.py
@@ -3,11 +3,9 @@ from ..colors import color
 
 class UpdatedEffects(dict):
 
-    def __init__(self, effects_dict: dict = None):
+    def __init__(self, effects_dict: dict[str, int] = None):
         effects_dict = effects_dict or {}
-
-        for effect_key, effect_ticks in effects_dict.items():
-            self[effect_key] = effect_ticks
+        self.load_from_dict(effects_dict)
 
     def add_effect(self, effect_key: str, effect_ticks: int):
         self[effect_key] = effect_ticks
@@ -20,7 +18,7 @@ class UpdatedEffects(dict):
     def has_effect(self, effect_key: str) -> bool:
         return effect_key in self
 
-    def update_effects(self):
+    def update_effects(self) -> None:
         keys_to_remove = []
         for effect_key, effect_ticks in self.items():
             self[effect_key] = effect_ticks - 1
@@ -30,8 +28,12 @@ class UpdatedEffects(dict):
         for effect_key in keys_to_remove:
             self.remove_effect(effect_key)
 
-    def to_dict(self) -> dict:
+    def to_dict(self) -> dict[str, int]:
         return dict(self)
+
+    def load_from_dict(self, effects_dict: dict[str, int]) -> None:
+        for effect_key, effect_ticks in effects_dict.items():
+            self[effect_key] = effect_ticks
 
     def __str__(self):
         if not self:
